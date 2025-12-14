74eefe41154c54336ce385295b63a91c2011145d
DD-gitdemo <dattamdudhabhate91@gmail.com>
2025-12-14 13:46:50 +0530

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
-sudo apt install httpd -y
+set -xe
+
+export DEBIAN_FRONTEND=noninteractive
+
+# Update packages
+apt-get update -y
+
+# Install Apache
+apt-get install -y apache2
+
+# Start & enable Apache
+systemctl start apache2
+systemctl enable apache2
